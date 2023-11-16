94f0ef854d3240ff36282db564c12dfcc50fc1da
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2023-11-16 16:47:53 +0100

    search bar sql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return React.createElement('div', null,
+    // Menu Bar
+    React.createElement('nav', null,
+      React.createElement('ul', null,
+        React.createElement('li', null, React.createElement('a', { href: '#' }, 'Home')),
+        React.createElement('li', null, React.createElement('a', { href: '#' }, 'About')),
+        React.createElement('li', null, React.createElement('a', { href: '#' }, 'Services')),
+        React.createElement('li', null, React.createElement('a', { href: '#' }, 'Contact'))
+      )
+    ),
+
+    // Search Option
+    React.createElement('div', { className: 'search-container' },
+      React.createElement('form', null,
+        React.createElement('input', { type: 'text', placeholder: 'Search...' }),
+        React.createElement('button', { type: 'submit' }, 'Search')
+      )
+    )
+
+    // Your application components and logic
   );
 }
 
