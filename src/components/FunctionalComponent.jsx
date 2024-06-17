0bf47b4ea8fceec2f4901d093a6ad316630eb965
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2024-06-17 22:42:31 +0200

    add functional comppnent

---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useRef } from 'react';
+import "@nightingale-elements/nightingale-sequence";
+import "@nightingale-elements/nightingale-navigation";
+import "@nightingale-elements/nightingale-manager";
+import "@nightingale-elements/nightingale-colored-sequence";
+import "@nightingale-elements/nightingale-track";
+import "@nightingale-elements/nightingale-structure";
+
+const FunctionalComponent = ({proteinData}) => {
+    const x = proteinData;
+    return <h3>GO Enrichment</h3>
+};
+
+export default FunctionalComponent;
