cf6f60a98e15c3d3a5adb99b0a8dff3af55ed082
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2024-11-19 20:45:51 +0100

    update home landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,17 +66,18 @@ function Home() {
           <img src="/images/dspalogo.webp" alt="DSPAtlas Logo" className="logo" />
         </div>
         <div className="dspa-text-blue">
-          <span className="dspa-jumbo dspa-hide-small">Dynamic Structural Proteome Atlas</span>
+          <span className="dspa-jumbo dspa-hide-small">DynaProt</span>
           <br />
-          <span className="dspa-xxlarge dspa-hide-large dspa-hide-medium">Dynamic Structural Proteome Atlas</span>
+          <span className="dspa-xxlarge dspa-hide-large dspa-hide-medium">DynaProt</span>
           <br />
-          <span className="dspa-large">Explore our comprehensive database.</span>
+          <span className="dspa-large">Explore dynamic changes in protein structure.</span>
         </div>
 
         <div className="three-boxes-container">
           {/* Left Box - Treatment Dropdown */}
           <div className="box">
             <label htmlFor="treatment-select">Select Treatment</label>
+            <p className="description">Choose a treatment condition to explore its impact on protein structures.</p>
             <select id="treatment-select" onChange={handleTreatmentChange}>
               <option value="osmotic_stress">Osmotic Stres</option>
               <option value="heatshock">Heatshock</option>
@@ -90,6 +91,7 @@ function Home() {
         <div className="box">
           <form onSubmit={handleSubmit}>
             <label htmlFor="protein-search">Protein Search</label>
+            <p className="description">Search for proteins by name to view related structural dynamics data.</p>
             <input
               id="protein-search"
               type="text"
@@ -103,12 +105,13 @@ function Home() {
         {/* Right Box - Experiment Link */}
         <div className="box">
           <label>Go to Experiments</label>
+          <p className="description">Explore all experiments related to protein structural changes.</p>
           <button onClick={() => navigate("/experiments")}>View Experiments</button>
         </div>
       </div>
 
         <div className="dspa-publications">
-          <h2>LiP Atlas Publications</h2>
+          <h2>Publications related to LiP-MS</h2>
           <div className="publication-block dspa-padding dspa-background-rose">
             <h3>
               Measuring protein structural changes on a proteome-wide scale using
@@ -126,6 +129,38 @@ function Home() {
               className="dspa-link"
             >
               View Publication on Nature.com
+            </a>
+            <h3>
+              Dynamic 3D proteomes reveal protein functional alterations at high resolution in situ
+            </h3>
+            <p>
+              Cappelletti V, Hauser T, Piazza I, Pepelnjak M, Malinovska L, Fuhrer T, 
+              Li Y, Dörig C, Boersema P, Gillet L, Grossbach J, Dugourd A, Saez-Rodriguez J, 
+              Beyer A, Zamboni N, Caflisch A, de Souza N,  Cell. 2021 Jan 21;184(2):545-559.e22. 
+              doi: 10.1016/j.cell.2020.12.021. Epub 2020 Dec 23. PMID: 33357446; PMCID: PMC7836100.
+            </p>
+            <a
+              href="https://www.sciencedirect.com/science/article/pii/S0092867420316913?via%3Dihub"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="dspa-link"
+            >
+              View Publication on sciencedirect.com
+            </a>
+            <h3>
+              A Map of Protein-Metabolite Interactions Reveals Principles of Chemical Communication
+            </h3>
+            <p>
+              Piazza I, Kochanowski K, Cappelletti V, Fuhrer T, Noor E, Sauer U, Picotti P. 
+              Cell. 2018 Jan 11;172(1-2):358-372.e23. doi: 10.1016/j.cell.2017.12.006. Epub 2018 Jan 4. PMID: 29307493.
+            </p>
+            <a
+              href="https://www.sciencedirect.com/science/article/pii/S0092867417314484?via%3Dihub"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="dspa-link"
+            >
+              View Publication on sciencedirect.com
             </a>
           </div>
         </div>
